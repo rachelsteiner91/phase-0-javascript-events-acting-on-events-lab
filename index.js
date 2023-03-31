580b51c3cf90e9326cc370e6ca20f9d7e6752fb0
Rachel Steiner <steinerr310@gmail.com>
2023-03-31 12:27:31 -0400

    javascript events complete

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+  function moveDodgerRight() {
+  var leftNumbers = dodger.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10);
+
+  if ( left < 360) {
+    dodger.style.left = `${left + 4}px`;
+  }
+}
+      
+document.addEventListener('keydown', function(e) {
+  if ( e.key === 37 ) {
+    moveDodgerLeft();
+  }
+  if ( e.key === 39 ) {
+    moveDodgerRight();
+  }
+});
